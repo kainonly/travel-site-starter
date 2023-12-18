ef46bba8bd7acdc09a80ecee61985a21bd1814da
kain <kainpro@foxmail.com>
2023-12-18 15:49:34 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { ConfigProvider, Layout, Menu, MenuProps } from 'antd';
+import { Layout, Menu, MenuProps } from 'antd';
 import React from 'react';
-
-import theme from '@/theme/themeConfig';
 
 const menus: MenuProps['items'] = ['1', '2', '3'].map(key => ({
   key,
@@ -10,19 +8,17 @@ const menus: MenuProps['items'] = ['1', '2', '3'].map(key => ({
 
 export default function Home() {
   return (
-    <ConfigProvider theme={theme}>
-      <Layout>
-        <Layout.Header style={{ display: 'flex', alignItems: 'center' }}>
-          <div className="demo-logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            items={menus}
-            style={{ flex: 1, minWidth: 0 }}
-          />
-        </Layout.Header>
-      </Layout>
-    </ConfigProvider>
+    <Layout>
+      <Layout.Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={menus}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Layout.Header>
+    </Layout>
   );
 }
