21574cbe339071357e5e63cc2aad739d5a009170
kain <kainpro@foxmail.com>
2023-12-23 12:00:46 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,87 @@
-import { Button } from 'antd';
-import { useState } from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, ConfigProvider, Layout, Menu, MenuProps, theme } from 'antd';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import './App.css';
+const items1: MenuProps['items'] = ['1', '2', '3'].map(key => ({
+  key,
+  label: `nav ${key}`
+}));
 
-function App() {
-  const [count, setCount] = useState(0);
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+  const key = String(index + 1);
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`
+      };
+    })
+  };
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>
+  }
+]);
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG }
+  } = theme.useToken();
 
   return (
-    <>
-      <div className="App">
-        <Button type="primary" onClick={() => setCount(count + 1)}>
-          Button {count}
-        </Button>
-      </div>
-    </>
+    <ConfigProvider>
+      <Layout style={{ height: '100%', overflow: 'hidden' }}>
+        <Layout.Header style={{ display: 'flex', alignItems: 'center' }}>
+          <div className="demo-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={items1}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </Layout.Header>
+
+        <Layout>
+          <Layout.Sider width={200} style={{ background: colorBgContainer }}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+              items={items2}
+            />
+          </Layout.Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Layout.Content
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG
+              }}
+            >
+              <RouterProvider router={router} />
+            </Layout.Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
   );
-}
+};
 
 export default App;
