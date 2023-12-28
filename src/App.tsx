d1b4b3ab8e681d8b2b86291be6adb532fdece908
kain <kainpro@foxmail.com>
2023-12-28 22:47:15 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,141 @@
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { ConfigProvider, Layout, Menu, MenuProps, theme } from 'antd';
+import {
+  IconBell,
+  IconBytedanceLogo,
+  IconHelpCircle,
+  IconHistogram,
+  IconHome,
+  IconLive,
+  IconSemiLogo,
+  IconSetting
+} from '@douyinfe/semi-icons';
+import { Avatar, Breadcrumb, Button, Layout, Nav, Skeleton } from '@douyinfe/semi-ui';
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { createBrowserRouter } from 'react-router-dom';
 
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-  const key = String(index + 1);
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`
-      };
-    })
-  };
-});
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello world!</div>
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <div>Hello world!</div>
+//   }
+// ]);
 
 const App: React.FC = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG }
-  } = theme.useToken();
-
   return (
-    <ConfigProvider>
-      <Layout style={{ height: '100%', overflow: 'hidden' }}>
-        <Layout.Header
-          style={{ display: 'flex', alignItems: 'center', background: '#fff', borderBottom: '1px solid #f0f0f0' }}
-        ></Layout.Header>
-
-        <Layout>
-          <Layout.Sider width={240} style={{ background: colorBgContainer, borderRight: '1px solid #f0f0f0' }}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{ height: '100%', borderRight: 0 }}
-              items={items2}
-            />
-          </Layout.Sider>
-          <Layout style={{ padding: '24px' }}>
-            <Layout.Content
+    <Layout style={{ height: '100%', border: '1px solid var(--semi-color-border)' }}>
+      <Layout.Header style={{ backgroundColor: 'var(--semi-color-bg-1)' }}>
+        <div>
+          <Nav mode="horizontal" defaultSelectedKeys={['Home']}>
+            <Nav.Header>
+              <IconSemiLogo style={{ height: '36px', fontSize: 36 }} />
+            </Nav.Header>
+            <span
               style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280,
-                background: colorBgContainer,
-                borderRadius: borderRadiusLG
+                color: 'var(--semi-color-text-2)'
               }}
             >
-              <RouterProvider router={router} />
-            </Layout.Content>
-          </Layout>
-        </Layout>
+              <span
+                style={{
+                  marginRight: '24px',
+                  color: 'var(--semi-color-text-0)',
+                  fontWeight: '600'
+                }}
+              >
+                模版推荐
+              </span>
+              <span style={{ marginRight: '24px' }}>所有模版</span>
+              <span>我的模版</span>
+            </span>
+            <Nav.Footer>
+              <Button
+                theme="borderless"
+                icon={<IconBell size="large" />}
+                style={{
+                  color: 'var(--semi-color-text-2)',
+                  marginRight: '12px'
+                }}
+              />
+              <Button
+                theme="borderless"
+                icon={<IconHelpCircle size="large" />}
+                style={{
+                  color: 'var(--semi-color-text-2)',
+                  marginRight: '12px'
+                }}
+              />
+              <Avatar color="orange" size="small">
+                YJ
+              </Avatar>
+            </Nav.Footer>
+          </Nav>
+        </div>
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider style={{ backgroundColor: 'var(--semi-color-bg-1)' }}>
+          <Nav
+            style={{ maxWidth: 220, height: '100%' }}
+            defaultSelectedKeys={['Home']}
+            items={[
+              { itemKey: 'Home', text: '首页', icon: <IconHome size="large" /> },
+              { itemKey: 'Histogram', text: '基础数据', icon: <IconHistogram size="large" /> },
+              { itemKey: 'Live', text: '测试功能', icon: <IconLive size="large" /> },
+              { itemKey: 'Setting', text: '设置', icon: <IconSetting size="large" /> }
+            ]}
+            footer={{
+              collapseButton: true
+            }}
+          />
+        </Layout.Sider>
+        <Layout.Content
+          style={{
+            padding: '24px',
+            backgroundColor: 'var(--semi-color-bg-0)'
+          }}
+        >
+          <Breadcrumb
+            style={{
+              marginBottom: '24px'
+            }}
+            routes={['首页', '当这个页面标题很长时需要省略', '上一页', '详情页']}
+          />
+          <div
+            style={{
+              borderRadius: '10px',
+              border: '1px solid var(--semi-color-border)',
+              height: '376px',
+              padding: '32px'
+            }}
+          >
+            <Skeleton placeholder={<Skeleton.Paragraph rows={2} />} loading={true}>
+              <p>Hi, Bytedance dance dance.</p>
+              <p>Hi, Bytedance dance dance.</p>
+            </Skeleton>
+          </div>
+        </Layout.Content>
       </Layout>
-    </ConfigProvider>
+      <Layout.Footer
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '20px',
+          color: 'var(--semi-color-text-2)',
+          backgroundColor: 'rgba(var(--semi-grey-0), 1)'
+        }}
+      >
+        <span
+          style={{
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          <IconBytedanceLogo size="large" style={{ marginRight: '8px' }} />
+          <span>Copyright © 2023 ByteDance. All Rights Reserved. </span>
+        </span>
+        <span>
+          <span style={{ marginRight: '24px' }}>平台客服</span>
+          <span>反馈建议</span>
+        </span>
+      </Layout.Footer>
+    </Layout>
   );
 };
 
