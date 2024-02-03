1d1057302eb45dce53333759a1df46890032ab6f
kain <kainpro@foxmail.com>
2024-02-03 18:14:42 +0800

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login).*)']
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|login).*)']
 };
