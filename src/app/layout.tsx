aa08d6e8d5272708365c190d7f3986bd93051155
kain <kainpro@foxmail.com>
2024-02-06 15:50:05 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'WEPLANX'
+  title: 'Hotel Sol'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
